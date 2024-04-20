24e080fa86628c124dc1f5452ca9ee84b4588277
Arik Alexandrov <arikxl@gmail.com>
2024-04-20 17:43:46 +0300

    init

---
+++ components/AppFooter.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppFooter = () => {
+  return (
+      <footer className='footer footer-center p-4 bg-base-300 text-base-content'>
+         &copy; Arik Alexandrov 2024 IL
+    </footer>
+  )
+}
+
+export default AppFooter
---
+++ components/AppHeader/AppHeader.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React from 'react'
+
+const AppHeader = () => {
+  return (
+    <header>
+      <nav>
+        <div className='navbar justify-between bg-base-300'>
+          <Link href='/' className='btn btn-ghost text-lg'>
+            NexCart
+          </Link>
+
+          <ul className='flex'>
+            <li>
+              <Link href='/cart' className='btn btn-ghost rounded-btn'>Cart</Link>
+            </li>
+            <li>
+              <Link href='/login' className='btn btn-ghost rounded-btn'>Login</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default AppHeader
